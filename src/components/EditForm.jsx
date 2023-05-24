17289aa4f1d358fe8eef477d2b08f84bc1c87701
Bishal Paul <bishalpaul34@gmail.com>
2023-05-24 23:02:57 +0530

    first commit update  user details

---
+++ src/components/EditForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EditForm() {
+  return (
+    <div>EditForm</div>
+  )
+}
+
+export default EditForm
--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import { doc, getDoc } from "firebase/firestore";
 import { db } from '../firebase'
+import EditForm from './EditForm';
 
 function UserPage(props) {
-  let userId=props.user.uid;
+  let userId = props.user.uid;
   const docRef = doc(db, "users", userId);
-  const [data,setData]=useState("");
-  
- async function getUserInfo(params) {
-   const docSnap = await getDoc(docRef);
-   if (docSnap.exists()) {
-    setData(docSnap.data());
-   } else {
-     // docSnap.data() will be undefined in this case
-     console.log("No such document!");
-   }
- }
- getUserInfo();
+  const [data, setData] = useState("");
+  const [edit,setEdit]=useState(false);
+
+  async function getUserInfo() {
+    useEffect(()=>{
+
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setData(docSnap.data());
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    },[])
+  }
+  getUserInfo();
   return (
     <>
       <h1>User Details:</h1>
       <p>Email: {props.user.email}</p>
       <p>Name: {data.name}</p>
       <p>Phone: {data.phone}</p>
-      <Button variant="primary" type="submit" >
-        Edit
-      </Button>
-    </>
+      <Button variant="primary" onClick={setEdit(true)}>Edit</Button>
+        {edit && <EditForm/>}
+   </>
   )
 }
 
