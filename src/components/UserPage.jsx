c83a68d5ff66ffd9c825a1990b2baa73361d5494
sanjeevminati <lg026013@gmail.com>
2023-05-26 19:49:16 +0530

    just testing

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from '../firebase'
-import EditForm from './EditForm';
+import { db } from "../firebase";
+import EditForm from "./EditForm";
 
 function UserPage(props) {
   let userId = props.user.uid;
@@ -22,19 +22,48 @@ function UserPage(props) {
   getUserInfo();
   return (
     <>
+      <div id="nav" className="bg-Cloket flex justify-start p-4">
+        <h1 className="font-syne font-semibold text-white text-5xl px-8 ">
+          CLOKET
+        </h1>
+        <ul className="flex gap-8 text-white font-syne font-semibold  items-center mx-20">
+          <li>Home</li>
+          <li>About US</li>
+          <li>INITIATIVE</li>
+          <li>Contact</li>
+          <li>Donate</li>
+        </ul>
+        <button
+          className="bg-white  rounded-full text-xl
+         w-28 mx-20 flex items-center justify-center"
+        >
+          Sign in
+        </button>
+      </div>
       <h1>User Details:</h1>
       <p>Email: {props.user.email}</p>
       <p>Name: {data.name}</p>
       <p>Phone: {data.phone}</p>
-      <Button variant="primary" onClick={() => setEdit(true)}>Edit</Button>
-      {edit &&
-        <EditForm
-          data={data}
-          setEdit={setEdit}
-          userId={userId}
-        />}
+      <button
+        type="submit"
+        className="bg-Cloket max-lg:w-36 sm:w-24 h-auto 
+        xl:text-xl sm:text-xs mx-2
+        font-syne
+        font-light
+        text-white  
+        p-3   rounded-sm  "
+        onClick={() => setEdit(true)}
+      >
+        NEXT
+      </button>
+      {edit && <EditForm data={data} setEdit={setEdit} userId={userId} />}
+      
+      {/* <Button variant="primary" onClick={() => setEdit(true)}>
+        Edit
+      </Button>
+      {edit && <EditForm data={data} setEdit={setEdit} userId={userId} />} */}
     </>
-  )
+  );
 }
 
-export default UserPage
+export default UserPage;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Cloket: "#7867df",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        syne: ["Syne", "sans-serif"],
+      },
+      width: {
+        600: "600px", // Replace 'custom' with your desired class name and '300px' with your desired width value
+      },
+      width: {
+        500: "510px",
+      },
+      width: {
+        12: "12px",
+      },
+      width: {
+        200: "202px",
+      },
+    },
+  },
+  plugins: [],
+};
