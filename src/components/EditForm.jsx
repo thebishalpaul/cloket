741911d0466db5e8acd62c015a9646c6ca56a8dc
sanjeevminati <lg026013@gmail.com>
2023-05-28 19:27:25 +0530

    Made a components of Navbar now edit button is due

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -5,17 +5,18 @@ function EditForm(props) {
   const [name, setName] = useState(props.data.name);
   const [phone, setPhone] = useState(props.data.phone);
   const [address, setAddress] = useState(props.data.address);
-  
+  const [showModal, setShowModal] = React.useState(false);
+
   function editDoc(params) {
     const ref = doc(db, "users", props.userId);
     updateDoc(ref, {
       name: params.name,
       phone: params.phone,
-      address: params.address
+      address: params.address,
     })
-    .then(() => {
-      console.log(
-        "A New Document Field has been added to an existing document"
+      .then(() => {
+        console.log(
+          "A New Document Field has been added to an existing document"
         );
       })
       .catch((error) => {
@@ -24,6 +25,54 @@ function EditForm(props) {
   }
   return (
     <>
+      {/* MODAL */}
+      {showModal ? (
+        <>
+          <div
+            className="justify-center font-syne items-center flex overflow-x-hidden 
+            overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div
+                className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white 
+                outline-none focus:outline-none"
+              >
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">EDIT PROFILE</h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  {/* {edit && (
+                    <EditForm data={data} setEdit={setEdit} userId={userId} />
+                  )} */}
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-Cloket text-white active:bg-purple-700 font-syne font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+      {/* MODAL */}
+      {/* MODAL */}
       <input
         type="text"
         name=""
@@ -31,7 +80,7 @@ function EditForm(props) {
         placeholder="name"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        />
+      />
       <input
         type="number"
         name=""
@@ -39,7 +88,7 @@ function EditForm(props) {
         placeholder="phone"
         value={phone}
         onChange={(e) => setPhone(e.target.value)}
-        />
+      />
       <input
         type="text"
         name=""
@@ -47,11 +96,11 @@ function EditForm(props) {
         placeholder="address"
         value={address}
         onChange={(e) => setAddress(e.target.value)}
-        />
+      />
       <button
         className="bg-Cloket text-white font-syne"
         onClick={() => {
-          editDoc({ name: name, phone: phone, address:address });
+          editDoc({ name: name, phone: phone, address: address });
           props.setEdit(false);
         }}
       >
--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -3,6 +3,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import EditForm from "./EditForm";
 import image from "./icon.png";
+import NavBar from "./NavBar";
 import { IoIosMenu } from "react-icons/io";
 import { BsCartFill } from "react-icons/fa";
 
@@ -12,11 +13,7 @@ function UserPage(props) {
   const [data, setData] = useState("");
   const [edit, setEdit] = useState(false);
   const [showModal, setShowModal] = React.useState(false);
-  const [isContentVisible, setIsContentVisible] = useState(false);
-
-  const toggleContent = () => {
-    setIsContentVisible(!isContentVisible);
-  };
+  
 
   async function getUserInfo() {
     const docSnap = await getDoc(docRef);
@@ -27,118 +24,65 @@ function UserPage(props) {
   useEffect(() => {
     getUserInfo();
   }, [data]);
-  const runBothFunctions = () => {
-    setShowModal(true);
-    setEdit(true);
-  };
-  // getUserInfo();
+  // const runBothFunctions = () => {
+  //   setShowModal(true);
+  //   setEdit(true);
+  // };
 
   return (
     <>
-      <div
-        id="nav"
-        className="bg-Cloket flex justify-start pt-4 pb-2 px-2 lg:p-4 w-full
-          lg:w-full 
-      "
-      >
-        <h1
-          className="font-syne font-semibold text-white text-2xl sm:px-8 px-4 
-        lg:text-5xl"
-        >
-          CLOKET
-        </h1>
-        <div className={`sm:flex ${isContentVisible ? "block" : "hidden"}`}>
-          <ul
-            className="flex font-syne font-semibold sm:items-center sm:gap-8   
-          flex-col sm:flex-row text-white"
-          >
-            <li className="py-2 sm:py-0 text-xs sm:text-lg sm:px-2">HOME</li>
-            <li className="py-2 sm:py-0 sm:px-2">BUY</li>
-            <li className="py-2 sm:py-0 sm:px-2">INITIATIVE</li>
-            <li className="py-2 sm:py-0 sm:px-2">CONTACT</li>
-            <li className="py-2 sm:py-0 sm:px-2">SWAP</li>
-            {/* <button className="bg-white text-Cloket 
-            w-auto sm:w-56 lg:flex mx-6 sm:mx-72">
-              LOGIN
-            </button> */}
-            <button
-              className=" w-3/5 h-6 sm:h-10  flex sm:block bg-white  
-              rounded-md text-xs lg:text-xl
-         lg:w-32  mx-6 sm:mx-44  lg:flex  items-center font-syne 
-         justify-center text-Cloket font-semibold"
-            >
-              CART
-            </button>
-          </ul>
-        </div>
+      <NavBar></NavBar>
 
-        {/* SHOW MENU : It shows only in small screen(Mobile) */}
-        <button
-          className="block sm:hidden font-syne rounded-md text-base 
-          cursor-pointer p-2 px-10 w-9 h-10  bg-white text-Cloket
-           mx-2"
-          onClick={toggleContent}
-        >
-          <IoIosMenu />
-        </button>
-
-        {/* MODAL */}
-        {showModal ? (
-          <>
-            <div
-              className="justify-center font-syne items-center flex overflow-x-hidden 
+      {/* MODAL */}
+      {/* {showModal ? (
+        <>
+          <div
+            className="justify-center font-syne items-center flex overflow-x-hidden 
             overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            >
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                {/*content*/}
-                <div
-                  className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white 
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl"> */}
+      {/*content*/}
+      {/* <div
+                className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white 
                 outline-none focus:outline-none"
-                >
-                  {/*header*/}
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">EDIT PROFILE</h3>
-                    <button
-                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={() => setShowModal(false)}
-                    >
-                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                        ×
-                      </span>
-                    </button>
-                  </div>
-                  {/*body*/}
-                  <div className="relative p-6 flex-auto">
-                    {edit && (
-                      <EditForm data={data} setEdit={setEdit} userId={userId} />
-                    )}
-                  </div>
-                  {/*footer*/}
-                  <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                    <button
-                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Close
-                    </button>
-                    <button
-                      className="bg-Cloket text-white active:bg-purple-700 font-syne font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Save Changes
-                    </button>
-                  </div>
+              > */}
+      {/*header*/}
+      {/* <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">EDIT PROFILE</h3>
+                 
+                </div> */}
+      {/*body*/}
+      {/* <div className="relative p-6 flex-auto">
+                  {edit && (
+                    <EditForm data={data} setEdit={setEdit} userId={userId} />
+                  )}
+                </div> */}
+      {/*footer*/}
+      {/* <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-Cloket text-white active:bg-purple-700 font-syne font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
                 </div>
               </div>
             </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-          </>
-        ) : null}
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null} */}
 
-        {/* MODAL */}
-      </div>
+      {/* MODAL */}
+
       <div className="main flex justify-center items-center gap-14 mt-16">
         <img src={image} className="w-40"></img>
         <div className="user-info flex flex-col font-syne font-bold  ">
@@ -152,16 +96,14 @@ function UserPage(props) {
           >
             {props.user.email}
           </p>
-          <p className="font-syne text-sm font-light w-60 ">
-            {data.address}
-          </p>
+          <p className="font-syne text-sm font-light w-60 ">{data.address}</p>
 
           <button
             type="submit"
             className="bg-Cloket 
             text-xl text-white w-4/12 
             mx-40  "
-            onClick={runBothFunctions}
+            //onClick={} ! Eikhane pass korte hobe
             // onClick={() => setShowModal(true) setEdit(true)}
             // // onClick={() => setEdit(true)}
           >
@@ -169,7 +111,6 @@ function UserPage(props) {
           </button>
         </div>
       </div>
-    
     </>
   );
 }
