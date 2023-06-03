070b97ab2e5a880efb46226055507a1f4060661c
sanjeevminati <lg026013@gmail.com>
2023-06-03 19:56:52 +0530

    6/3/2023

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -80,7 +80,7 @@ function UserPage(props) {
       <div
         className="main flex sm:flex-row   w-full justify-center
        items-center sm:gap-0 gap-4 sm:mx-0  
-       -mx-4 mt-16"
+       -mx-4 sm:mt-16"
       >
         <div className=" relative flex flex-col mb-1 sm:w-56">
           {/* Profile Picture */}
@@ -124,7 +124,7 @@ function UserPage(props) {
 
         {/*-----MAIN------*/}
 
-        <div className="user-info flex flex-col -mx-2 font-syne font-bold ">
+        <div className="user-info flex flex-col mt-5  -mx-2 font-syne font-bold ">
           <p className="font-syne text-sm sm:text-4xl mb-1">{data.name}</p>
           <p className="font-syne  text-xs sm:text-2xl mb-1 text-Cloket">
             +91-{data.phone}
@@ -145,9 +145,9 @@ function UserPage(props) {
         >
           <button
             type="submit"
-            className="bg-Cloket 
+            className="bg-Cloket
              text-white sm:w-auto w-auto  text-xs p-1 sm:text-xl  
-            sm:p-1 rounded-lg "
+            sm:p-1 rounded-lg mt-10"
             onClick={runBothFunctions}
           >
             <AiFillEdit className="mr-0" />
@@ -163,6 +163,76 @@ function UserPage(props) {
               // getUserInfo={getUserInfo}
             />
           )}
+        </div>
+      </div>
+      {/* ----------------- */}
+
+      {/* STATUS SECTION */}
+
+      <div className="flex mt-10  justify-center h-screen">
+        <div className="mx-28 w-3/6 bg-purple-500 p-0 flex flex-col">
+          <div className="flex justify-between flex-row p-3">
+            <h1 className="font-syne ml-0  font-semibold text-white text-2xl">
+              STATUS #0000
+            </h1>
+            <button
+              className="bg-white text-Cloket font-syne font-semibold
+          rounded-md w-auto h-10 mr-2 px-3 text-xs"
+            >
+              SELECT ITEM
+            </button>
+          </div>
+          <div
+            className="bg-purple-300 mt-2 flex flex-col
+           items-center justify-center w-full p-2"
+          >
+            <h1 className="font-syne font-bold  text-lg text-white">
+              Verification Sucessfully completed
+            </h1>
+            <p className="font-syne font-light text-white">11/5/2022</p>
+          </div>
+          <div
+            className="bg-purple-300 mt-2 flex flex-col
+           items-center justify-center w-full p-2"
+          >
+            <h1 className="font-syne font-bold  text-lg text-white">
+              Price evaluation completed
+            </h1>
+            <p className="font-syne font-light text-white">11/5/2022</p>
+          </div>
+          {/* FOOTER */}
+          <div className="bg-purple-300 mt-2 flex flex-col  w-full p-2">
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="font-syne font-bold text-lg text-white">
+                Ready to List online
+              </h1>
+              <p className="font-syne font-light text-white">11/5/2022</p>
+            </div>
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-col p-4 justify-start items-start">
+                <p className="font-syne font-light text-base text-white mb-0">
+                  Offer Price:
+                </p>
+                <h1 className="font-syne font-bold text-lg -mt-1 text-white">
+                  70 swap coins
+                </h1>
+              </div>
+              <div className="flex flex-row mt-0 gap-3 p-4">
+                <button
+                  className="bg-green-400 text-black font-syne font-semibold
+          rounded-md h-10 flex items-center  p-3 "
+                >
+                  Accept
+                </button>
+                <button
+                  className="bg-red-600 text-black font-syne font-semibold
+          rounded-md h-10 flex items-center  p-3 "
+                >
+                  Reject
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
